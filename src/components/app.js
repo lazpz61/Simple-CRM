34aaf15f72f918ad2c957bcc0534b259980db212
LazaroMPerez <lazpz61@gmail.com>
2021-03-23 00:03:19 -0400

    second commit

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,10 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>Laz's Simple CRM Project</h1>
+        
       </div>
+    
     );
   }
 }
